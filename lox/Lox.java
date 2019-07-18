28bf3ab3080b940214b6eadc4f51d3db8b9b7348
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-07-18 17:03:11 -0600

    Replace the printing of the expression with the interpreter!

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
   static boolean hadRuntimeError = false;
 
@@ -53,7 +54,7 @@ public class Lox {
     // Stop if there was a syntax error
     if (hadError) return;
 
-    System.out.println(new Astprinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
