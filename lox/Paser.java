cc80c1306bbef558787f7dc6f1ac9579b0eb8da2
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-07-18 22:53:17 -0600

    Block scoping/execution

--- lox/Paser.java
+++ lox/Paser.java
@@ -15,7 +15,7 @@ class Parser {
   }
 
   List<Stmt> parse() {
-    List<Stmt> statements = new ArrayList();
+    List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
       statements.add(declaration());
     }
@@ -38,6 +38,8 @@ class Parser {
 
   private Stmt statement() {
     if (match(PRINT)) return printStatement();
+    if (match(LEFT_BRACE)) return new Stmt.Block(block());
+
     return expressionStatement();
   }
 
@@ -63,6 +65,17 @@ class Parser {
     Expr expr = expression();
     consume(SEMICOLON, "Expect ';' after expression.")
     return new Stmt.Expression(expr);
+  }
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
   }
 
   private Expr assignment() {
